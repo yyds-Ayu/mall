f7c6f7fbd090c565d15771ea82c46ef5a3abe33a
Ayu <1760347458@qq.com>
2021-08-22 16:44:57 +0800

    第三次修改

--- src/main/java/com/baizhi/yyds/yyds1/ms.java
+++ src/main/java/com/baizhi/yyds/yyds1/ms.java
@@ -4,8 +4,10 @@ public class ms {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int c = 30；
+        int c = 30;
+        int d = 40;
         System.out.println(a+b);
         System.out.println(c);
+        System.out.println(c+d);
     }
 }
