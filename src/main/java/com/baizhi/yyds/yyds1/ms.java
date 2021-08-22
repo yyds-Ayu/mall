9ee496a86f48a20efc1ba517197c656144f4f98c
Lid_Ayu <89233470+yyds-Ayu@users.noreply.github.com>
2021-08-22 16:24:12 +0800

    Update ms.java
    第二次修改大妈

--- src/main/java/com/baizhi/yyds/yyds1/ms.java
+++ src/main/java/com/baizhi/yyds/yyds1/ms.java
@@ -4,6 +4,8 @@ public class ms {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
+        int c = 30；
         System.out.println(a+b);
+        System.out.println(c);
     }
 }
